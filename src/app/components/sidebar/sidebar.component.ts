38f1d4d6899422f4cb2b781b489a982ec1e8602c
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-10 04:19:50 -0400

    Generando modulo linea

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
+  { path: '/dashboard', title: 'lineas', icon: 'design_app', class: '' },
+  /**
     { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
     { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
@@ -16,6 +18,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
     { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
+     */
 
 ];
 
@@ -33,9 +36,9 @@ export class SidebarComponent implements OnInit {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ( window.innerWidth > 991) {
-          return false;
-      }
-      return true;
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
   };
 }
---
+++ src/app/linea/linea.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LineaRoutingModule } from './linea-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LineaRoutingModule
+  ]
+})
+export class LineaModule { }
