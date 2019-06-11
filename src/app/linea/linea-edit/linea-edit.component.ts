86137745bdc67ecc566cd9188f000cf268fa45dc
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-11 10:53:03 -0400

    creando paradas y vagones

--- src/app/linea/linea-edit/linea-edit.component.ts
+++ src/app/linea/linea-edit/linea-edit.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LineaService } from '../../shared/services/linea.service';
 import { Linea } from '../../shared/models/linea';
+import { Validators, FormBuilder } from '@angular/forms';
+import { Parada } from '../../shared/models/parada';
+import { Vagon } from '../../shared/models/vagon';
 
 @Component({
   selector: 'app-linea-edit',
@@ -17,9 +20,35 @@ export class LineaEditComponent implements OnInit {
     paradas: []
   };
 
+  vagonForm = this.formBuilder.group({
+    id: [0],
+    numero: [0,],
+    cantidad: [0, [Validators.required]],
+
+  });
+
+  paradaForm = this.formBuilder.group({
+    id: [0],
+    origen: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+    destino: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+    tiempo: [0, [Validators.required]],
+  });
+
+  createPrada() {
+    let parada: Parada = this.paradaForm.value;
+    this.linea.paradas.push(parada);
+
+  }
+  createVagon() {
+    let vagon: Vagon = this.vagonForm.value;
+    vagon.numero = this.linea.vagones.length + 1;
+    this.linea.vagones.push(vagon);
+  }
+
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
-    private lineaService: LineaService) { }
+    private lineaService: LineaService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(
