53d14708482b434e03741aa510e45bd0a7570c90
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-13 10:10:18 -0400

    creando panel para seleccionar vagones

--- src/app/linea/linea-simulacion/linea-simulacion.component.ts
+++ src/app/linea/linea-simulacion/linea-simulacion.component.ts
@@ -62,200 +62,200 @@ export class LineaSimulacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.chartColor = "#FFFFFF";
-    this.canvas = document.getElementById("bigDashboardChart");
-    this.ctx = this.canvas.getContext("2d");
+    // this.chartColor = "#FFFFFF";
+    // this.canvas = document.getElementById("bigDashboardChart");
+    // this.ctx = this.canvas.getContext("2d");
 
-    this.gradientStroke = this.ctx.createLinearGradient(500, 0, 100, 0);
-    this.gradientStroke.addColorStop(0, '#80b6f4');
-    this.gradientStroke.addColorStop(1, this.chartColor);
+    // this.gradientStroke = this.ctx.createLinearGradient(500, 0, 100, 0);
+    // this.gradientStroke.addColorStop(0, '#80b6f4');
+    // this.gradientStroke.addColorStop(1, this.chartColor);
 
-    this.gradientFill = this.ctx.createLinearGradient(0, 200, 0, 50);
-    this.gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
-    this.gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
+    // this.gradientFill = this.ctx.createLinearGradient(0, 200, 0, 50);
+    // this.gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+    // this.gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
 
-    this.lineBigDashboardChartData = [
-      {
-        label: "Data",
+    // this.lineBigDashboardChartData = [
+    //   {
+    //     label: "Data",
 
-        pointBorderWidth: 1,
-        pointHoverRadius: 7,
-        pointHoverBorderWidth: 2,
-        pointRadius: 5,
-        fill: true,
+    //     pointBorderWidth: 1,
+    //     pointHoverRadius: 7,
+    //     pointHoverBorderWidth: 2,
+    //     pointRadius: 5,
+    //     fill: true,
 
-        borderWidth: 2,
-        data: [50, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95]
-      }
-    ];
-    this.lineBigDashboardChartColors = [
-      {
-        backgroundColor: this.gradientFill,
-        borderColor: this.chartColor,
-        pointBorderColor: this.chartColor,
-        pointBackgroundColor: "#2c2c2c",
-        pointHoverBackgroundColor: "#2c2c2c",
-        pointHoverBorderColor: this.chartColor,
-      }
-    ];
-    this.lineBigDashboardChartLabels = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-    this.lineBigDashboardChartOptions = {
+    //     borderWidth: 2,
+    //     data: [50, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95]
+    //   }
+    // ];
+    // this.lineBigDashboardChartColors = [
+    //   {
+    //     backgroundColor: this.gradientFill,
+    //     borderColor: this.chartColor,
+    //     pointBorderColor: this.chartColor,
+    //     pointBackgroundColor: "#2c2c2c",
+    //     pointHoverBackgroundColor: "#2c2c2c",
+    //     pointHoverBorderColor: this.chartColor,
+    //   }
+    // ];
+    // this.lineBigDashboardChartLabels = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    // this.lineBigDashboardChartOptions = {
 
-      layout: {
-        padding: {
-          left: 20,
-          right: 20,
-          top: 0,
-          bottom: 0
-        }
-      },
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: '#fff',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
-        bodySpacing: 4,
-        xPadding: 12,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest"
-      },
-      legend: {
-        position: "bottom",
-        fillStyle: "#FFF",
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: "rgba(255,255,255,0.4)",
-            fontStyle: "bold",
-            beginAtZero: true,
-            maxTicksLimit: 5,
-            padding: 10
-          },
-          gridLines: {
-            drawTicks: true,
-            drawBorder: false,
-            display: true,
-            color: "rgba(255,255,255,0.1)",
-            zeroLineColor: "transparent"
-          }
+    //   layout: {
+    //     padding: {
+    //       left: 20,
+    //       right: 20,
+    //       top: 0,
+    //       bottom: 0
+    //     }
+    //   },
+    //   maintainAspectRatio: false,
+    //   tooltips: {
+    //     backgroundColor: '#fff',
+    //     titleFontColor: '#333',
+    //     bodyFontColor: '#666',
+    //     bodySpacing: 4,
+    //     xPadding: 12,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest"
+    //   },
+    //   legend: {
+    //     position: "bottom",
+    //     fillStyle: "#FFF",
+    //     display: false
+    //   },
+    //   scales: {
+    //     yAxes: [{
+    //       ticks: {
+    //         fontColor: "rgba(255,255,255,0.4)",
+    //         fontStyle: "bold",
+    //         beginAtZero: true,
+    //         maxTicksLimit: 5,
+    //         padding: 10
+    //       },
+    //       gridLines: {
+    //         drawTicks: true,
+    //         drawBorder: false,
+    //         display: true,
+    //         color: "rgba(255,255,255,0.1)",
+    //         zeroLineColor: "transparent"
+    //       }
 
-        }],
-        xAxes: [{
-          gridLines: {
-            zeroLineColor: "transparent",
-            display: false,
+    //     }],
+    //     xAxes: [{
+    //       gridLines: {
+    //         zeroLineColor: "transparent",
+    //         display: false,
 
-          },
-          ticks: {
-            padding: 10,
-            fontColor: "rgba(255,255,255,0.4)",
-            fontStyle: "bold"
-          }
-        }]
-      }
-    };
+    //       },
+    //       ticks: {
+    //         padding: 10,
+    //         fontColor: "rgba(255,255,255,0.4)",
+    //         fontStyle: "bold"
+    //       }
+    //     }]
+    //   }
+    // };
 
-    this.lineBigDashboardChartType = 'line';
+    // this.lineBigDashboardChartType = 'line';
 
 
-    this.gradientChartOptionsConfiguration = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      tooltips: {
-        bodySpacing: 4,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest",
-        xPadding: 10,
-        yPadding: 10,
-        caretPadding: 10
-      },
-      responsive: 1,
-      scales: {
-        yAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }],
-        xAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }]
-      },
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 15,
-          bottom: 15
-        }
-      }
-    };
+    // this.gradientChartOptionsConfiguration = {
+    //   maintainAspectRatio: false,
+    //   legend: {
+    //     display: false
+    //   },
+    //   tooltips: {
+    //     bodySpacing: 4,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest",
+    //     xPadding: 10,
+    //     yPadding: 10,
+    //     caretPadding: 10
+    //   },
+    //   responsive: 1,
+    //   scales: {
+    //     yAxes: [{
+    //       display: 0,
+    //       ticks: {
+    //         display: false
+    //       },
+    //       gridLines: {
+    //         zeroLineColor: "transparent",
+    //         drawTicks: false,
+    //         display: false,
+    //         drawBorder: false
+    //       }
+    //     }],
+    //     xAxes: [{
+    //       display: 0,
+    //       ticks: {
+    //         display: false
+    //       },
+    //       gridLines: {
+    //         zeroLineColor: "transparent",
+    //         drawTicks: false,
+    //         display: false,
+    //         drawBorder: false
+    //       }
+    //     }]
+    //   },
+    //   layout: {
+    //     padding: {
+    //       left: 0,
+    //       right: 0,
+    //       top: 15,
+    //       bottom: 15
+    //     }
+    //   }
+    // };
 
-    this.gradientChartOptionsConfigurationWithNumbersAndGrid = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      tooltips: {
-        bodySpacing: 4,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest",
-        xPadding: 10,
-        yPadding: 10,
-        caretPadding: 10
-      },
-      responsive: true,
-      scales: {
-        yAxes: [{
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawBorder: false
-          }
-        }],
-        xAxes: [{
-          display: 0,
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            zeroLineColor: "transparent",
-            drawTicks: false,
-            display: false,
-            drawBorder: false
-          }
-        }]
-      },
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 15,
-          bottom: 15
-        }
-      }
-    };
+    // this.gradientChartOptionsConfigurationWithNumbersAndGrid = {
+    //   maintainAspectRatio: false,
+    //   legend: {
+    //     display: false
+    //   },
+    //   tooltips: {
+    //     bodySpacing: 4,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest",
+    //     xPadding: 10,
+    //     yPadding: 10,
+    //     caretPadding: 10
+    //   },
+    //   responsive: true,
+    //   scales: {
+    //     yAxes: [{
+    //       gridLines: {
+    //         zeroLineColor: "transparent",
+    //         drawBorder: false
+    //       }
+    //     }],
+    //     xAxes: [{
+    //       display: 0,
+    //       ticks: {
+    //         display: false
+    //       },
+    //       gridLines: {
+    //         zeroLineColor: "transparent",
+    //         drawTicks: false,
+    //         display: false,
+    //         drawBorder: false
+    //       }
+    //     }]
+    //   },
+    //   layout: {
+    //     padding: {
+    //       left: 0,
+    //       right: 0,
+    //       top: 15,
+    //       bottom: 15
+    //     }
+    //   }
+    // };
 
     /**lineChartExample */
     // this.canvas = document.getElementById("lineChartExample");
