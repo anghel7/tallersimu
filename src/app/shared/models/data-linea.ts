cb1b3a302c60360bb66c743fb4939feb3a846201
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-17 20:32:16 -0400

    Creacion del modelo data-linea

---
+++ src/app/shared/models/data-linea.ts
@@ -0,0 +1,10 @@
+import { GraphicPoint } from './graphic-point';
+
+export interface DataLinea {
+ datameses: GraphicPoint[];
+ datahoras: GraphicPoint[];
+ datasemanas: GraphicPoint[];
+ datadias: GraphicPoint[];
+ nombre: string;
+ idlinea: number;
+};
