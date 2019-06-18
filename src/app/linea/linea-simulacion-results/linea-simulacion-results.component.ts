cbb894c254f95aac7c34f8b6f0d4ede41fee62c7
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-18 07:05:57 -0400

    Graficando tablas faltantens

--- src/app/linea/linea-simulacion-results/linea-simulacion-results.component.ts
+++ src/app/linea/linea-simulacion-results/linea-simulacion-results.component.ts
@@ -22,6 +22,9 @@ export class LineaSimulacionResultsComponent implements OnInit {
       .subscribe(
         (response: DataLinea[]) => {
           this.setupDataMeses(response);
+          this.setupDataSemanas(response);
+          this.setupDataDias(response);
+          this.setupDataHoras(response);
         }
       );
   }
@@ -52,7 +55,86 @@ export class LineaSimulacionResultsComponent implements OnInit {
     }
   }
 
+  private setupDataSemanas(data: DataLinea[]): any {
+    this.dataSemanas = {
+      headers: [],
+      columns: []
+    };
+    let aux: any[] = [];
+    this.dataSemanas.headers.push("");
+    data.forEach(dataLinea => {
+      this.dataSemanas.headers.push(dataLinea.nombre);
+      aux.push(dataLinea.datasemanas);
+    });
+    if (aux.length >= 1) {
+      for (let index = 0; index < aux[0].length; index++) {
+        const element0 = aux[0][index];
+        const element1 = aux[1][index];
+        const element2 = aux[2][index];
+        let arrayItem: any[] = [];
+        arrayItem.push(element0.time);
+        arrayItem.push(element0.value);
+        arrayItem.push(element1.value);
+        arrayItem.push(element2.value);
+        this.dataSemanas.columns.push(arrayItem)
+      }
+    }
+  }
+
+  private setupDataDias(data: DataLinea[]): any {
+    this.dataDias = {
+      headers: [],
+      columns: []
+    };
+    let aux: any[] = [];
+    this.dataDias.headers.push("");
+    data.forEach(dataLinea => {
+      this.dataDias.headers.push(dataLinea.nombre);
+      aux.push(dataLinea.datadias);
+    });
+    if (aux.length >= 1) {
+      for (let index = 0; index < aux[0].length; index++) {
+        const element0 = aux[0][index];
+        const element1 = aux[1][index];
+        const element2 = aux[2][index];
+        let arrayItem: any[] = [];
+        arrayItem.push(element0.time);
+        arrayItem.push(element0.value);
+        arrayItem.push(element1.value);
+        arrayItem.push(element2.value);
+        this.dataDias.columns.push(arrayItem)
+      }
+    }
+  }
+
+  private setupDataHoras(data: DataLinea[]): any {
+    this.dataHoras = {
+      headers: [],
+      columns: []
+    };
+    let aux: any[] = [];
+    this.dataHoras.headers.push("");
+    data.forEach(dataLinea => {
+      this.dataHoras.headers.push(dataLinea.nombre);
+      aux.push(dataLinea.datahoras);
+    });
+    if (aux.length >= 1) {
+      for (let index = 0; index < aux[0].length; index++) {
+        const element0 = aux[0][index];
+        const element1 = aux[1][index];
+        const element2 = aux[2][index];
+        let arrayItem: any[] = [];
+        arrayItem.push(element0.time);
+        arrayItem.push(element0.value);
+        arrayItem.push(element1.value);
+        arrayItem.push(element2.value);
+        this.dataHoras.columns.push(arrayItem)
+      }
+    }
+  }
+
 }
+
 export interface DataTable {
   headers: string[],
   columns: any[][],
