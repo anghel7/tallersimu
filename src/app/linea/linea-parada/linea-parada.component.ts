a93b09c59c7200e3a4a3864935c20d293f33538c
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-06-18 10:05:44 -0400

    visualizando paradas de las lineas

--- src/app/linea/linea-parada/linea-parada.component.ts
+++ src/app/linea/linea-parada/linea-parada.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LineaService } from '../../shared/services/linea.service';
+import { Linea } from '../../shared/models/linea';
 
 @Component({
   selector: 'app-linea-parada',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineaParadaComponent implements OnInit {
 
-  constructor() { }
+  lineas: Linea[] = [];
+
+  constructor(private lineaService: LineaService) { }
 
   ngOnInit() {
+    this.lineaService.getAll()
+      .subscribe(
+        (response) => {
+          this.lineas = response;
+        }
+      );
   }
 
 }
